1f047bfd1504d66e8c2888593eb00c1bd94297c7
PanCytryn <pancytryn.dawid@gmail.com>
2024-10-21 11:16:48 +0200

    Skończona kartkówka

---
+++ js/script.js
@@ -0,0 +1,65 @@
+function one(){
+    const div = document.createElement("button");
+    const input = document.createElement("input");
+    input.type = "color";
+    div.style.width = "100px";
+    div.style.height = "100px";
+    document.body.appendChild(div);
+    document.body.appendChild(input);
+    input.addEventListener("input", ()=>{
+        div.style.backgroundColor = input.value;
+    })
+}
+
+function two(){
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    button.textContent = "Generuj";
+    document.body.appendChild(input);
+    document.body.appendChild(button);
+    button.addEventListener("click", ()=>{
+        const a = document.createElement("a");
+        a.textContent = `https://${input.value}`; 
+        a.setAttribute("href", `https://${input.value}`); 
+        document.body.appendChild(a);
+    })
+}
+
+function three(){
+    const button = document.createElement("button");
+    button.textContent = "Stwórz"
+    const inputWidth = document.createElement("input");
+    const inputHeight = document.createElement("input");
+    document.body.appendChild(button);
+    document.body.appendChild(inputWidth);
+    document.body.appendChild(inputHeight);
+    const div = document.createElement("div")
+    div.style.border = "2px solid black"
+
+    button.addEventListener("click", ()=>{
+        document.body.appendChild(div)
+        div.style.width = `${inputWidth.value}px`;
+        div.style.height = `${inputHeight.value}px`;
+    })
+}
+
+function four(){
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    const ul = document.createElement("ul");
+    document.body.appendChild(input);
+    document.body.appendChild(button);
+    document.body.appendChild(ul);
+    button.textContent = "Dodaj";
+    button.addEventListener("click", ()=>{
+        const li = document.createElement("li");
+        li.textContent = input.value;
+        const buttonRemove = document.createElement("button");
+        buttonRemove.textContent = "DELETE";
+        buttonRemove.addEventListener("click", function (){
+            this.parentElement.style.display = "none";
+        })
+        ul.appendChild(li);
+        li.appendChild(buttonRemove);
+    })
+}
